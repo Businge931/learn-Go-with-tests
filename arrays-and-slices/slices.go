1b33c65af5ee1685ef4684f5fff381063e317194
Businge931 <jimmybusinge9@gmail.com>
2024-08-01 09:59:23 +0300

    feat: add SumAll func body

--- arrays-and-slices/slices.go
+++ arrays-and-slices/slices.go
@@ -8,3 +8,13 @@ func SumForSlices(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = SumForSlices(numbers)
+	}
+	return sums
+}
