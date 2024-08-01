053de7abc82a19e4224d9127f4596a994cb3f473
Businge931 <jimmybusinge9@gmail.com>
2024-08-01 10:00:00 +0300

    feat: added test case for SumAll func

--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -1,6 +1,9 @@
 package arraysandslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -15,7 +18,7 @@ func TestSum(t *testing.T) {
 	})
 
 	t.Run("collection of any size", func(t *testing.T) {
-		numbers := []int{1, 2, 3,4}
+		numbers := []int{1, 2, 3, 4}
 
 		got := SumForSlices(numbers)
 		want := 10
@@ -24,4 +27,13 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+	
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
