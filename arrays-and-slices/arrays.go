bbee355971247d9d33d38d33de3c4a72398b7e6f
Businge931 <jimmybusinge9@gmail.com>
2024-08-01 08:22:20 +0300

    fix: renamed func

--- arrays-and-slices/arrays.go
+++ arrays-and-slices/arrays.go
@@ -1,6 +1,8 @@
 package arraysandslices
 
-func Sum(numbers [5]int) int {
+
+// Arrays are declared with a fixed length WHILE slices are declared with any length 
+func SumForArrays(numbers [5]int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
